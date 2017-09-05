bf5835f4823f988eea2cc7675ea1e037b7f18db3
Vitaliy <vitaliy@mage2cloud.com>
2017-09-05 10:46:41 +0300

    Few improvements for easytabs widget

--- Block/WidgetTabs.php
+++ Block/WidgetTabs.php
@@ -6,6 +6,9 @@ class WidgetTabs extends Tabs implements \Magento\Widget\Block\BlockInterface
     protected function _getCollection()
     {
         $collection = parent::_getCollection();
+        $filterTabs = str_replace(' ', '', $this->getFilterTabs());
+        $filterTabs = explode(',', $filterTabs);
+        $collection->addFieldToFilter('alias', array('in' => $filterTabs));
         $collection->addWidgetTabFilter();
         return $collection;
     }
