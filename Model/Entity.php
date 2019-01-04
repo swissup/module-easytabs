a7537533b7c15d08777580c75f137466f4287e3c
Alexander Kras'ko <0m3r.mail@gmail.com>
2019-01-04 14:31:42 +0200

    Fix previous commit

--- Model/Entity.php
+++ Model/Entity.php
@@ -363,6 +363,11 @@ class Entity extends \Magento\Framework\Model\AbstractModel
      */
     public function getStores()
     {
-        return $this->hasData('stores') ? $this->getData('stores', []) : $this->getData('store_id', []);
+        $stores = $this->hasData('stores') ? $this->getData('stores') : $this->getData('store_id');
+
+        if (!is_array($stores)) {
+            $stores = (array) $stores;
+        }
+        return $stores;
     }
 }
