df11f27b8ddd4da75a3ea5879de614eb41f39759
Vitaliy Dmytruk <vitaliy@mage2cloud.com>
2019-12-02 10:37:06 +0200

    Code cleanup. Remove redundant files.

--- Ui/Component/Listing/Column/WidgetActions.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Swissup\Easytabs\Ui\Component\Listing\Column;
-
-class WidgetActions extends EasytabsActions
-{
-    /** Url path */
-    const URL_PATH_EDIT = 'easytabs/widget/edit';
-    const URL_PATH_DELETE = 'easytabs/widget/delete';
-}
