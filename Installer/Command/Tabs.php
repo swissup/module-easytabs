ba3f8230f8561d13643a52ad5a018a51277f42ff
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2020-02-11 16:33:50 +0200

    Unlink existing tabs and then create new when using Installer

--- Installer/Command/Tabs.php
+++ Installer/Command/Tabs.php
@@ -10,16 +10,40 @@ class Tabs
     private $logger;
 
     /**
+     * @var \Magento\Framework\Stdlib\DateTime\TimezoneInterface
+     */
+    private $localeDate;
+
+    /**
+     * @var \Magento\Store\Model\StoreManager
+     */
+    private $storeManager;
+
+    /**
+     * @var \Swissup\Easytabs\Model\EntityFactory
+     */
+    private $tabFactory;
+
+    /**
      * @var \Swissup\Easytabs\Model\ResourceModel\Entity\CollectionFactory
      */
     private $tabsCollectionFactory;
 
     /**
+     * @param \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate
+     * @param \Magento\Store\Model\StoreManager $storeManager
+     * @param \Swissup\Easytabs\Model\EntityFactory $tabFactory
      * @param \Swissup\Easytabs\Model\ResourceModel\Entity\CollectionFactory $tabsCollectionFactory
      */
     public function __construct(
+        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
+        \Magento\Store\Model\StoreManager $storeManager,
+        \Swissup\Easytabs\Model\EntityFactory $tabFactory,
         \Swissup\Easytabs\Model\ResourceModel\Entity\CollectionFactory $tabsCollectionFactory
     ) {
+        $this->localeDate = $localeDate;
+        $this->storeManager = $storeManager;
+        $this->tabFactory = $tabFactory;
         $this->tabsCollectionFactory = $tabsCollectionFactory;
     }
 
@@ -41,32 +65,57 @@ class Tabs
     {
         $this->logger->info('Easytabs: Create tabs');
 
+        $isSingleStoreMode = $this->storeManager->isSingleStoreMode();
+
         foreach ($request->getParams() as $data) {
-            $tab = $this->tabsCollectionFactory
+            $tabs = $this->tabsCollectionFactory
                 ->create()
                 ->addFieldToFilter('alias', $data['alias'])
                 ->addFieldToFilter('block', $data['block'])
-                ->getFirstItem();
+                ->addStoreFilter($request->getStoreIds());
 
-            if ($tab->getId()) {
-                $storeIds = array_unique(
-                    array_merge($tab->getStores(), $request->getStoreIds())
-                );
+            foreach ($tabs as $tab) {
+                $storesToLeave = array_diff($tab->getStores(), $request->getStoreIds());
 
-                if (!array_diff($storeIds, $tab->getStores())) {
-                    // tab is already assigned to requested store
-                    continue;
+                if (count($storesToLeave) && !$isSingleStoreMode) {
+                    $tab->setStores($storesToLeave);
+                } else {
+                    $tab->setStatus(0)
+                        ->setAlias($this->getBackupAlias($tab->getAlias()));
                 }
-            } else {
-                $tab->setData($data);
-                $storeIds = $request->getStoreIds();
+
+                try {
+                    $tab->save();
+                } catch (\Exception $e) {
+                    $this->logger->warning($e->getMessage());
+                }
             }
 
+            $data = array_merge([
+                'status' => 1,
+            ], $data);
+
             try {
-                $tab->setStores($storeIds)->save();
+                $this->tabFactory->create()
+                    ->setData($data)
+                    ->setStores($request->getStoreIds())
+                    ->save();
             } catch (\Exception $e) {
                 $this->logger->warning($e->getMessage());
             }
         }
+    }
+
+    /**
+     * @param string $alias
+     * @return string
+     */
+    private function getBackupAlias($alias)
+    {
+        return $alias
+            . '_backup_'
+            . rand(10, 99)
+            . '_'
+            . $this->localeDate->date()->format('Y-m-d-H-i-s');
     }
 }
