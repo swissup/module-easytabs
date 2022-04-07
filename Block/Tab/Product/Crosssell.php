fed264b3eeeb84681e52e78332826310c52f8586
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2022-04-07 15:39:53 +0300

    Added ability to show crosssell products in product page tabs. Closes #23

---
+++ Block/Tab/Product/Crosssell.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Swissup\Easytabs\Block\Tab\Product;
+
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Catalog\ViewModel\Product\Listing\PreparePostData;
+use Magento\Framework\App\ObjectManager;
+
+class Crosssell extends \Magento\Checkout\Block\Cart\Crosssell
+{
+    public function getItems()
+    {
+        $items = $this->getData('items');
+        if ($items !== null) {
+            return $items;
+        }
+
+        $items = [];
+        $currentProduct = $this->getProduct();
+        if ($currentProduct) {
+            $collection = $this->_getCollection()->addProductFilter(
+                $currentProduct->getData($this->getProductLinkField())
+            );
+
+            $collection->setPositionOrder()->load();
+
+            foreach ($collection as $item) {
+                $items[] = $item;
+            }
+        }
+
+        $this->setData('items', $items);
+
+        return $items;
+    }
+
+    protected function _prepareLayout()
+    {
+        if (class_exists(PreparePostData::class)) {
+            $this->setViewModel(
+                ObjectManager::getInstance()->create(PreparePostData::class)
+            );
+        }
+    }
+
+    private function getProductLinkField(): string
+    {
+        return ObjectManager::getInstance()->get(CollectionFactory::class)->create()
+            ->getProductEntityMetadata()
+            ->getLinkField();
+    }
+}
