b43e10de5e581ee32bd9f5b19cb4d4f512b6874d
Vitaliy Dmytruk <vitaliy@mage2cloud.com>
2022-09-28 10:26:46 +0300

    Fix unable to select product attribute from dropdown in admin when tab "Product attribute".

---
+++ Block/Form/Element/Renderer/UiSelect.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Swissup\Easytabs\Block\Form\Element\Renderer;
+
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class UiSelect extends UiComponent
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function initJsLayout(AbstractElement $element)
+    {
+        $value = $element->getValue();
+        $config = $this->getComponentConfig();
+        $fieldName = $config['fieldName'] ?? false;
+
+        if ($fieldName) {
+            $value = $value[$fieldName] ?? [];
+        }
+
+        if (!$value) {
+            $value = [];
+        }
+
+        $element->setValue($value);
+
+        return parent::initJsLayout($element);
+    }
+}
